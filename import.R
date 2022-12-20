3e3b683126cf76a92c14e7f1f145bed1d6e3c00b
Ajahn Jhanarato <jhanarato@gmail.com>
2022-12-20 09:26:12 +0800

    Named columns and omitted unavailable columns.

--- import.R
+++ import.R
@@ -1,11 +1,25 @@
 library(tidyverse)
 
 read_weather_cloud_file <- function(file_name) {
-  read_delim(file_name, delim=";", locale = readr::locale(encoding = "UTF-16LE"))
+  columns <- c("date_time", "temp_inside", "temp", "chill", "dew_inside", "dew", 
+               "heat_inside", "heat", "humidity_inside", "humidity", 
+               "windspeed_high", "wind_speed_avg", "wind_direction_avg", 
+               "pressure_hpa", "rainfall", "rainfall_rate", "solar_rad", 
+               "uv", "elipsis_nineteen")
+  
+  unavailable <- c("dew_inside", "heat_inside", "heat", 
+                   "solar_rad", "uv", "elipsis_nineteen")
+  
+  read_delim(file_name, 
+             delim=";",
+             skip=1,
+             locale = readr::locale(encoding = "UTF-16LE"),
+             col_names = columns,
+             col_select = !unavailable
+             )
 }
 
 import_weather <- function() {
   files <- list.files(pattern="*.csv")
   bind_rows(map(files, read_weather_cloud_file))
 }
-
