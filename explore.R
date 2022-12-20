2ae2909cf73d2b8d34857d97c7182f9d6ef10ff1
Ajahn Jhanarato <jhanarato@gmail.com>
2022-12-20 09:40:52 +0800

    Exploring begins!

--- explore.R
+++ explore.R
@@ -1,3 +1,15 @@
 source("import.R")
 
+library(lubridate)
+
 weather <- import_weather()
+
+weather %>% arrange(desc(temp))
+
+weather %>%
+  group_by(year(date_time), month(date_time)) %>%
+  summarise(
+    max_temp = max(temp, na.rm=TRUE)
+  )
+
+number_of_observations <- weather %>% count(year(date_time), month(date_time, label=TRUE))
